5c971d32e53a548b5506020f17e79ee128c07784
dabouti <d.h.kim@wustl.edu>
2025-07-11 16:54:36 -0500

    Add Leads Tracker and Car Comparison app files
    A car Comparison app with brand/model/year selection, car data fetching, and an AI chat assistant. Includes all related HTML, CSS, JS, images, and configuration files.

---
+++ CarComparison/ai.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let messageContainer = document.getElementById("message-container");
+    let sendBtn = document.getElementById("send");
+    let userInput = document.getElementById("user-input");
+
+    sendBtn.addEventListener("click", async () => {
+        const message = userInput.value;
+        if (!message) {
+            return;
+        }
+
+        addMessage(message, "User");
+        message.value = "";
+        await sendMessage(message);
+
+        loadingScreen();
+    })
+})
+
+
+async function sendMessage(myMessage) {
+    try {
+        const response = await fetch("http://localhost:3000/api/chat",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ message: myMessage })
+            })
+        if (!response.ok) {
+            throw new Error()
+        }
+        removeLoading();
+        const data = await response.json();
+        addMessage(data.response, "AI")
+    }
+    catch (error) {
+        addMessage("Sorry, something went wrong!", "AI");
+    }
+}
+
+async function addMessage(message, sender) {
+    const messageBox = document.createElement("div");
+    if (sender === "AI") {
+        messageBox.className = "bot-message";
+    }
+    else {
+        messageBox.className = "user-message"
+    }
+    messageBox.innerHTML = `<strong>${sender}</strong>${message}</h3>`
+    messageContainer.appendChild(messageBox);
+}
+
+function loadingScreen() {
+    const loading = document.createElement("div");
+    loading.id = "loading";
+    loading.innerHTML = `<strong>AI: </strong>Thinking, please wait....</h3>`;
+    messageContainer.appendChild(loading);
+}
+
+function removeLoading() {
+    const loading = document.getElementById("loading");
+    loading.remove();
+}
